4f47c3f619ae3b8d0b321af167f73318a85c7586
Philipp Rosenberger <p.rosenberger@kunbus.com>
2020-10-28 10:41:36 +0100

    Build the image before running the command
    Signed-off-by: Philipp Rosenberger <p.rosenberger@kunbus.com>

--- builddeb-raspian-stretch.sh
+++ builddeb-raspian-stretch.sh
@@ -6,4 +6,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 export FLAVOUR="raspbian-stretch"
 
+( cd "$DIR" && docker build -t "builddeb-$FLAVOUR" . )
+
 exec "$DIR/builddeb.sh" "$@"
